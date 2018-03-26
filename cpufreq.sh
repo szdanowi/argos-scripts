0e7a130ff8422d7bf27672c9a4615543abdfaf6a
Sebastian Zdanowicz <14982968+szdanowi@users.noreply.github.com>
2018-03-26 20:39:15 +0200

    cpufreq script added

---
+++ cpufreq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! "`command -v cpupower`" ]; then
+    echo "fail: \`cpupower\` missing | color=red"
+    echo "---"
+    echo "Please install \`cpupower\` tool ... | href='https://linux.die.net/man/1/cpupower'"
+    exit
+fi
+
+current_frequency=`cpupower frequency-info -fm | grep "frequency:" | sed -e 's/.*: *//g' | sed -e 's/ *(.*//g'`
+current_governor=`cpupower frequency-info | grep "may decide" | sed -e 's/.*"\(.*\)".*/\1/g'`
+available_governors=`cpupower frequency-info -g | grep "governors:" | sed -e 's/.*: //g'`
+switch_governor_command="sudo cpupower frequency-set -g"
+
+echo "${current_frequency}"
+echo "---"
+
+for governor in ${available_governors}
+do
+    if [[ "${governor}" == "${current_governor}" ]]; then
+        prefix="<b>"
+        postfix=" •</b>"
+    fi
+    echo "${prefix}${governor}${postfix} | terminal=false bash=\"${switch_governor_command} ${governor}\""
+done
+
